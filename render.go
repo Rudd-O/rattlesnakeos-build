0160e71cc340b6c8db49e06e497e5e3107e57db6
Manuel Amador (Rudd-O) <rudd-o@rudd-o.com>
2019-02-08 10:02:42 +0000

    Test for the correct file.

--- render.go
+++ render.go
@@ -107,7 +107,7 @@ set -x
 			`# Suppressed copy from S3 to external/prebuilt/arm64/ as this happens later`,
 			-1,
 		},
-		{`fetch --nohooks android`, `test -f .gclient || fetch --nohooks android`, -1},
+		{`fetch --nohooks android`, `test -d src/.git || fetch --nohooks android`, -1},
 		{
 			`yes | gclient sync --with_branch_heads --jobs 32 -RDf
 
